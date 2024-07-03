dac57d783ec8bd7b4fdc353ebed55dc3326bd5b2
Jan Chruszcz <jrchruszcz@gmail.com>
2024-07-03 15:16:51 +0200

    Tests after turbo streaming

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
-require 'rails_helper'
-
 RSpec.describe "Dashboard", type: :request do
   
   describe "GET /" do
-    it "displays the list of todos as sticky notes" do
-      list = List.create(title: "Groceries")
+    it "displays lists of todos" do
+      list1 = List.create(title: "Groceries")
+      list2 = List.create(title: "Home")
 
-      todo1 = list.todos.create(title: "Milk")
-      todo2 = list.todos.create(title: "Cornflakes")
+      todo1 = list1.todos.create(title: "Milk")
+      todo2 = list1.todos.create(title: "Cornflakes")
+
+      todo3 = list2.todos.create(title: "Clean the house")
+      todo4 = list2.todos.create(title: "Wash the car")
 
       get "/"
       expect(response).to have_http_status(200)
       expect(response.body).to include(todo1.title)
       expect(response.body).to include(todo2.title)
+      expect(response.body).to include(todo3.title)
+      expect(response.body).to include(todo4.title)
     end
   end
 
@@ -26,12 +30,22 @@ RSpec.describe "Dashboard", type: :request do
         post "/lists", params: { list: list_params }
       }.to change(List, :count).by(1)
 
-      expect(response).to redirect_to("/")
-      follow_redirect!
-
       expect(response).to have_http_status(200)
       expect(response.body).to include(list_params[:title])
 
+    end
+  end
+
+  describe "DELETE /lists/:id" do
+    it "deletes an existing list" do
+      list = List.create(title: "Home")
+      
+      expect {
+        delete "/lists/#{list.id}"
+      }.to change(List, :count).by(-1)
+
+      expect(response).to have_http_status(200)
+      expect(response.body).not_to include(list.title)
     end
   end
 
@@ -44,9 +58,6 @@ RSpec.describe "Dashboard", type: :request do
       expect {
         post "/todos", params: { todo: todo_params }
       }.to change(Todo, :count).by(1)
-
-      expect(response).to redirect_to("/")
-      follow_redirect!
 
       expect(response).to have_http_status(200)
       expect(response.body).to include(todo_params[:title])
@@ -61,8 +72,6 @@ RSpec.describe "Dashboard", type: :request do
       updated_title = "Oranges"
 
       patch "/todos/#{todo.id}", params: { todo: { title: updated_title } }
-      expect(response).to redirect_to("/")
-      follow_redirect!
 
       expect(response).to have_http_status(200)
       expect(response.body).to include(updated_title)
@@ -78,9 +87,6 @@ RSpec.describe "Dashboard", type: :request do
       expect {
         delete "/todos/#{todo.id}"
       }.to change(Todo, :count).by(-1)
-
-      expect(response).to redirect_to("/")
-      follow_redirect!
 
       expect(response).to have_http_status(200)
       expect(response.body).not_to include(todo.title)
