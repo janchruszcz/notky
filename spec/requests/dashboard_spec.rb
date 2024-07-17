9821c9224b2a74a31b82b064c7646e19f0625ee3
Jan Chruszcz <jrchruszcz@gmail.com>
2024-07-17 15:33:40 +0200

    Dashboard request spec

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Dashboard", type: :feature do
-  
-  describe "GET /" do
-    it "displays lists of todos" do
-      list1 = List.create(title: "Groceries")
-      list2 = List.create(title: "Home")
+RSpec.describe 'Dashboard', type: :request do
+  describe 'GET /' do
+    it 'displays lists of todos' do
+      list1 = List.create(title: 'Groceries')
+      list2 = List.create(title: 'Home')
 
-      todo1 = list1.todos.create(title: "Milk")
-      todo2 = list1.todos.create(title: "Cornflakes")
+      todo1 = list1.todos.create(title: 'Milk')
+      todo2 = list1.todos.create(title: 'Cornflakes')
 
-      todo3 = list2.todos.create(title: "Clean the house")
-      todo4 = list2.todos.create(title: "Wash the car")
+      todo3 = list2.todos.create(title: 'Clean the house')
+      todo4 = list2.todos.create(title: 'Wash the car')
 
-      get "/"
+      get '/'
       expect(response).to have_http_status(200)
       expect(response.body).to include(todo1.title)
       expect(response.body).to include(todo2.title)
